3a6f8a86332746dee579f5b7ba7062e1babc1eca
Jeff Kriske <jekriske@gmail.com>
2013-11-10 15:16:35 -0500

    have userlist check if logged on

--- userlist.php
+++ userlist.php
@@ -2,7 +2,10 @@
 session_start();
 include_once("connection.php");
 include_once("common_functions.php");
-
+if (!$_SESSION['userid']) {
+    header("Location: login.php");
+    exit;
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
