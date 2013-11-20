11f21cfa684b097c009187b3c5fccac360243cc5
jekriske <jekriske@gmail.com>
2013-11-20 01:16:30 -0500

    tables

--- mySQL_tables.sql
+++ mySQL_tables.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 15, 2013 at 02:57 AM
+-- Generation Time: Nov 20, 2013 at 01:15 AM
 -- Server version: 5.1.69
 -- PHP Version: 5.3.3
 
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `hashtags` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   KEY `hashtag` (`hashtag`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
 -- --------------------------------------------------------
 
@@ -44,14 +44,14 @@ CREATE TABLE IF NOT EXISTS `hashtags` (
 CREATE TABLE IF NOT EXISTS `roars` (
   `body` varchar(720) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `lat` double NOT NULL COMMENT 'Latitude',
-  `lng` double NOT NULL COMMENT 'Longitude',
+  `lat` double NOT NULL DEFAULT '0' COMMENT 'Latitude',
+  `lng` double NOT NULL DEFAULT '0' COMMENT 'Longitude',
   `users_id` int(10) NOT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   KEY `users_id` (`users_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=61 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=67 ;
 
 -- --------------------------------------------------------
 
@@ -66,7 +66,7 @@ CREATE TABLE IF NOT EXISTS `sheep` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   KEY `fk_sheep_users1_idx` (`users_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 -- --------------------------------------------------------
 
@@ -83,7 +83,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
